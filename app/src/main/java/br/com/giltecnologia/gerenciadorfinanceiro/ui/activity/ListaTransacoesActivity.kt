8006a340ebc1650ecbcca89bd2b0e9e8f69b052b
Ney Marques <neyrpg@gmail.com>
2017-12-14 22:02:39 -0200

    Inclusão de dialog para adicionar transação

--- app/src/main/java/br/com/giltecnologia/gerenciadorfinanceiro/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/giltecnologia/gerenciadorfinanceiro/ui/activity/ListaTransacoesActivity.kt
@@ -1,12 +1,14 @@
 package br.com.giltecnologia.gerenciadorfinanceiro.ui.activity
 
 import android.app.DatePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.DatePicker
 import android.widget.Toast
 import  br.com.giltecnologia.gerenciadorfinanceiro.R
@@ -17,23 +19,26 @@ import br.com.giltecnologia.gerenciadorfinanceiro.model.Transacao
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
+
+    private val transacoes: MutableList<Transacao> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
         //findViewById<ListView>(R.id.lista_transacoes_listview)//
 
-        val lista = transacoesExemplo()
+        //val lista = transacoesExemplo()
 
         // val adapter = ArrayAdapter( this, android.R.layout.simple_list_item_1, lista)
 
-        lista_transacoes_listview.adapter = TransacoesAdapter(this, lista)
+        atualizarList()
 
-        configuraResumo(lista)
+        configuraResumo()
 
         lista_transacoes_adiciona_receita.setOnClickListener {
 
@@ -42,28 +47,66 @@ class ListaTransacoesActivity : AppCompatActivity() {
                     window.decorView as ViewGroup,
                     false
             )
+
+            val ano = 2017
+            val mes = 11
+            val dia = 12
+
             //viewDialog.form_transacao_valor.
             viewDialog.form_transacao_data.setText(Calendar.getInstance().formatoBrasileiro())
             viewDialog.form_transacao_data.setOnClickListener{
-                DatePickerDialog(this, DatePickerDialog.OnDateSetListener() {
-                    datePicker: DatePicker, i: Int, i1: Int, i2: Int ->
-
-                },2017, 12, 10)
+                DatePickerDialog(this, DatePickerDialog.OnDateSetListener { datePicker, ano, mes, dia ->
+                val dataSelecionada = Calendar.getInstance()
+                    dataSelecionada.set(ano,mes,dia)
+                    viewDialog.form_transacao_data.setText(dataSelecionada.formatoBrasileiro())
+                },ano,mes,dia).show()
             }
+
+            val adapterCategoria = ArrayAdapter.createFromResource(this, R.array.categorias_de_receita, android.R.layout.simple_spinner_dropdown_item)
+            viewDialog.form_transacao_categoria.adapter = adapterCategoria
+
 
            AlertDialog.Builder(this)
                    .setTitle(R.string.adiciona_receita)
                    .setView(viewDialog)
+                   .setNegativeButton("Cancelar",null)
+                   .setPositiveButton("Adicionar", DialogInterface.OnClickListener { dialogInterface, i ->
+                       val valor = viewDialog.form_transacao_valor.text.toString()
+                       val data = viewDialog.form_transacao_data.text.toString()
+                       val categoria = viewDialog.form_transacao_categoria.selectedItem.toString()
+
+                       val valorBigDecimal = BigDecimal(valor)
+                       val sdf = SimpleDateFormat("dd/MM/yyyy")
+                       val dataFormatada = sdf.parse(data)
+
+                       val dataConvertida = Calendar.getInstance()
+                       dataConvertida.time = dataFormatada
+
+                       val transacao = Transacao(tipo = TipoTransacaoEnum.RECEITA, valor = valorBigDecimal, data = dataConvertida, categoria = categoria)
+                       //Toast.makeText(this,"${transacao.categoria} - ${transacao.valor}", Toast.LENGTH_LONG).show()
+                       adicionarTransacao(transacao)
+                       lista_transacoes_adiciona_menu.close(true)
+                   })
                    .show()
         }
 
     }
 
-    private fun configuraResumo(lista: List<Transacao>) {
+    private fun adicionarTransacao(transacao: Transacao) {
+        transacoes.add(transacao)
+        configuraResumo()
+        atualizarList()
+    }
+
+    private fun atualizarList() {
+        lista_transacoes_listview.adapter = TransacoesAdapter(this, transacoes)
+    }
+
+    private fun configuraResumo() {
         val viewResumo = window.decorView
-        ResumoView(viewResumo, lista).adicionaDespesa()
-        ResumoView(viewResumo, lista).adicionaReceita()
-        ResumoView(viewResumo, lista).adicionaTotal()
+        ResumoView(viewResumo, transacoes).adicionaDespesa()
+        ResumoView(viewResumo, transacoes).adicionaReceita()
+        ResumoView(viewResumo, transacoes).adicionaTotal()
 
     }
 
